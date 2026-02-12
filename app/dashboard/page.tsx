3ba52cdf4bb8045ba18bc4cadc505c7e171ee763
Neevs1 <neevan.redkar@gmail.com>
2026-02-12 22:32:07 +0530

    Added placeholders for prep and interview pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 export default function dashboard(){
     const router = useRouter();
     const [msg,setMsg] = useState("");
+    const [usermail,setUsermail] = useState("");
 
     useEffect(() => {
         //If there is no user in local storage, redirect to / i.e. our landing page
@@ -30,12 +31,28 @@ export default function dashboard(){
         router.push("/");
 
     }
+    useEffect(() => {
+        
+        setUsermail(localStorage.getItem("user")!);
+    },[]);
 
     return(
-        <div>
-            <h1>Hello World!</h1>
+        <div className="bg-[#030712]">
+            <div className="min-w-screen min-h-20 bg-[ #111827] mt-10 font-sans">
+                <h1 className="p-4 text-4xl">Welcome {usermail}</h1>
+            </div>
+            
             <p>{msg}</p>
-            <button onClick={logout}>Logout</button>
+            <button className="bg-[#2563eb] hover:bg-[#0ea5e9]" onClick={logout}>Logout</button>
+            <button className="bg-[#2563eb] hover:bg-[#0ea5e9]" onClick={logout}>Interview phase</button>
+            <h3>Or revise these topics</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div><p>Aptitude</p></div>
+                <div><p>Data Structures & Algorithms </p></div>
+                <div><p>Object oriented programming</p></div>
+                <div><p>Database Management</p></div>
+                <div><p>Networking</p></div>
+            </div>
         </div>
     )
 }
---
+++ app/interview/page.tsx
@@ -0,0 +1,7 @@
+export default function interviewpage(){
+    return(
+        <div>
+            <h1>Welcome to interview page</h1>
+        </div>
+    )
+}
